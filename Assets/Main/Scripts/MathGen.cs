adfbfc2911b7b17bfe450ad712cbc90a8fd7fa0e
AbdulGinem <abdul_ginem@yahoo.com>
2021-01-29 08:22:50 -0600

    adding multiple chocie

--- Assets/Main/Scripts/MathGen.cs
+++ Assets/Main/Scripts/MathGen.cs
@@ -2,19 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MathGen : MonoBehaviour { 
 
-   int a;
-   int b;
-   public GameObject Question;
- 
+    int a;
+    int b;
+    public GameObject Question;
+    private int score;
+    private int Answer;
 
-    void Start()
+    public GameObject OptionB;
+    public GameObject OptionC;
+    public GameObject OptionD;
+    
+
+    public List<GameObject> optionsList = new List<GameObject>();
+    GameObject choosedOption;
+
+
+
+    void ChooseRandomOption()
+    {
+        choosedOption = optionsList[Random.Range(0, optionsList.Count)];
+    }
+
+
+
+    public void UserStart()
     
     {
+        score = 0;
+        Answer = a + b;
         a = Random.Range(0, 10);
         b = Random.Range(0, 10);
+        NextQuestion();
+
+    }
+
+    public void NextQuestion()
+    {
+
+        Debug.Log(choosedOption);
+        
     }
 
     void Update()
