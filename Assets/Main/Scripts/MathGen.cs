f481d247238254ef25487fbbdcda9f546eab795c
Abdul <34360387+coolabudlg19@users.noreply.github.com>
2021-01-29 18:51:11 -0600

    MathGen Edit

--- Assets/Main/Scripts/MathGen.cs
+++ Assets/Main/Scripts/MathGen.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Diagnostics;
+
 
 public class MathGen : MonoBehaviour { 
 
@@ -12,36 +14,36 @@ public class MathGen : MonoBehaviour {
     private int score;
     private int Answer;
 
+    public GameObject OptionA;
     public GameObject OptionB;
     public GameObject OptionC;
     public GameObject OptionD;
     
-
-    public List<GameObject> optionsList = new List<GameObject>();
-    GameObject choosedOption;
-    void ChooseRandomOption()
+    void Start()
     {
-        choosedOption = optionsList[Random.Range(0, optionsList.Count)];
+        optionsList.Add(OptionA);
+        optionsList.Add(OptionB);
+        optionsList.Add(OptionC);
+        optionsList.Add(OptionD);
+        
+        
     }
 
 
+    List<GameObject> optionsList = new List<GameObject>();
+    GameObject choosedOption;
 
+  
     public void UserStart()
     
     {
+
         score = 0;
         Answer = a + b;
         a = Random.Range(0, 10);
         b = Random.Range(0, 10);
-        NextQuestion();
+        choosedOption = optionsList[Random.Range(0, optionsList.Count -1)];
 
-    }
-
-    public void NextQuestion()
-    {
-
-        Debug.Log(choosedOption);
-        
     }
 
     void Update()
